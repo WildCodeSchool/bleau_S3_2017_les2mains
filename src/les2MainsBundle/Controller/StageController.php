486d9d26445c071d2a68fa0e77d91114a1330bb7
Cecile_Berger <ccl.berger@gmail.com>
2017-05-13 22:41:54 +0200

    Create page stage for user

---
+++ src/les2MainsBundle/Controller/StageController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace les2MainsBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request.
+
+class StageController extends Controller
+{
+    public function stageAction()
+    {
+        return $this->render('@les2Mains/User/nos_stages.html.twig');
+    }
+
+    public function addAction(Request $request){
+
+        $stage = new Stage();
+        $form = $this->CreateForm();
+
+    }
+}
