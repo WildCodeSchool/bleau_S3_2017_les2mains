9a5a2349fba1fd46835ecc8e915355c3ac7b96ca
Cecile_Berger <ccl.berger@gmail.com>
2017-05-15 10:04:42 +0200

    error fix

--- src/les2MainsBundle/Controller/StageController.php
+++ src/les2MainsBundle/Controller/StageController.php
@@ -3,7 +3,7 @@
 namespace les2MainsBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request.
+use Symfony\Component\HttpFoundation\Request;
 
 class StageController extends Controller
 {
