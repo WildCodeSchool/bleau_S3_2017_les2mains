e063b5aa9d0fea059f65ff4684e7f6977bd61801
SebiLob <solomon.grundy.51@gmail.com>
2017-05-18 11:47:27 +0200

    final scss

--- src/CoreBundle/Resources/public/js/stage.js
+++
@@ -1,6 +0,0 @@
-// modal de la page "stage" pour le form
-
-$(document).ready(function(){
-    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-    $('.modal').modal();
-});
