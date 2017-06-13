9f50922554167d15b87ce22c86b4ca2017452fcd
JIM2022 <jaymail04@gmail.com>
2017-06-13 14:58:36 +0200

    Edit Form1 fonctionnel

--- src/CoreBundle/Controller/OurController.php
+++ src/CoreBundle/Controller/OurController.php
@@ -14,45 +14,35 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 class OurController extends Controller
 {
 
-    /*public function ourFormAction()
-    {
-        // On crée un objet Article
-        $our_page = new Our();
-
-        // On crée le FormBuilder grâce à la méthode du contrôleur
-        $formBuilder = $this->createFormBuilder($our_page);
-
-        // On ajoute les champs de l'entité que l'on veut à notre formulaire
-        $formBuilder
-            ->add('titre1',         TextareaType::class)
-            ->add('contenu1',    TextareaType::class)
-            ->add('save_1',         SubmitType::class)
-
-            ->add('titre2',         TextareaType::class)
-            ->add('contenu2',    TextareaType::class)
-            ->add('save_2',         SubmitType::class)
-
-            ->add('titre3',         TextareaType::class)
-            ->add('contenu3',    TextareaType::class)
-            ->add('save_3',         SubmitType::class)
-            ;
-
-        // À partir du formBuilder, on génère le formulaire
-        $form = $formBuilder->getForm();
-
-        // On passe la méthode createView() du formulaire à la vue afin qu'elle puisse afficher le formulaire toute seule
-        return $this->render('@Core/pages/nous.html.twig', array(
-            'form' => $form->createView(),
-        ));
-    }*/
-    public function ourFormAction()
+    public function ourFormAction(Request $request)
     {
        $em=$this->getDoctrine()->getManager();
        $ourContent=$em->getRepository(Our::class)->myFindSingleOurPage();
 
+        // On crée le FormBuilder grâce à la méthode du contrôleur
+        $formbuilder = $this->createFormBuilder($ourContent);
+
+        // On ajoute les champs de l'entité que l'on veut à notre formulaire
+        $formbuilder
+            ->add('titre1', TextareaType::class)
+            ->add('contenu1', TextareaType::class)
+            ->add('save_1', SubmitType::class);
+
+        // À partir du formBuilder, on génère le formulaire
+        $form1 = $formbuilder->getForm();
+        $form1->handleRequest($request);
+
+        // On vérifie qu'elle est de type POST
+        if( $request->isMethod('post'))
+        {
+            $em->persist($ourContent);
+            $em->flush();
+        }
+
 
        return $this->render('@Core/pages/nous.html.twig', array(
-           'our' => $ourContent
+           'our' => $ourContent,
+           'form1' => $form1->createView()
        ));
     }
 
@@ -61,32 +51,6 @@ class OurController extends Controller
 
 
 
-    /*public function nousAction()
-    {
-        $our_page = new Our();
-
-        $formBuilder = $this->createFormBuilder($our_page);
-
-        $formBuilder
-            ->add('titre1',         TextareaType::class)
-            ->add('contenu1',    TextareaType::class)
-            ->add('save_1',         SubmitType::class)
-
-            ->add('titre2',         TextType::class)
-            ->add('contenu2',       TextareaType::class)
-            ->add('save_2',         SubmitType::class)
-
-            ->add('titre3',         TextareaType::class)
-            ->add('contenu3',    TextareaType::class)
-            ->add('save_3',         SubmitType::class)
-        ;
-
-        $form = $formBuilder->getForm();
-
-        return $this->render('@Core/pages/nous.html.twig', array(
-            'form' => $form->createView(),
-        ));
-    }*/
 
 
 
