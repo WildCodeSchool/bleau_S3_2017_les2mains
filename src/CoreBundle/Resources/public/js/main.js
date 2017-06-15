34dd6c40bed033fb2872e5e764ff0604318248d5
KyoLuma <couchotcynthia99@gmail.com>
2017-06-15 17:09:34 +0200

    administration done

--- src/CoreBundle/Resources/public/js/main.js
+++ src/CoreBundle/Resources/public/js/main.js
@@ -65,3 +65,11 @@ $('.open-overlay').click(function() {
         })
     }
 });
+
+/* Date Picker */
+
+
+$('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15 // Creates a dropdown of 15 years to control year
+});
