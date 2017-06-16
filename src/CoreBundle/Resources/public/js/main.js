bf6a841c261de40afaf2e2a0d6adcf26397353cf
SebiLob <solomon.grundy.51@gmail.com>
2017-06-16 12:04:22 +0200

    menu home auto 3 sec

--- src/CoreBundle/Resources/public/js/main.js
+++ src/CoreBundle/Resources/public/js/main.js
@@ -78,3 +78,10 @@ $('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
     selectYears: 15 // Creates a dropdown of 15 years to control year
 });
+
+
+// Menu Nav Fermeture Auto
+
+$(document).ready(function () {
+
+});
