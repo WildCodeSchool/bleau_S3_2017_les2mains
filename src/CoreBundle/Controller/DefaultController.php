931e5f0bf6fb5c4d8ef2ee65d42dc41c2a76fc8d
SebiLob <solomon.grundy.51@gmail.com>
2017-06-19 17:06:20 +0200

    swift-mailer OK page contact

--- src/CoreBundle/Controller/DefaultController.php
+++ src/CoreBundle/Controller/DefaultController.php
@@ -10,59 +10,32 @@ use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
+    /**
+     * Render homage
+     * @return Response
+     */
     public function indexAction()
     {
         return $this->render('CoreBundle:pages:index.html.twig');
     }
 
-   /* public function contactAction(Request $request)
-    {
-        $contact = new Contact();
-        $form = $this->createForm(ContactType::class, $contact);
-
-        $form->handleRequest($request);
-
-        if($form->isSubmitted() && $form->isValid())
-        {
-            $em=$this->getDoctrine()->getManager();
-            $em->persist($contact);
-            $em->flush();
-
-            return $this->redirectToRoute('core_contact');
-        }
-
-
-        return $this->render('@Core/pages/contact.html.twig',array(
-            'form'=> $form->createView()
-        ));
-    }*/
-
+    /**
+     * Render contact page
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|Response
+     */
     public function contactAction(Request $request)
     {
-        // Create the form according to the FormType created previously.
-        // And give the proper parameters
-
         $contact = new Contact();
         $form = $this->createForm(ContactType::class, $contact);
 
-        if ($request->isMethod('POST')) {
-            // Refill the fields in case the form is not valid.
-            $form->handleRequest($request);
+        // Refill the fields in case the form is not valid.
+        $form->handleRequest($request);
 
-            if($form->isValid()){
-                // Send mail
-                if($this->sendEmail($form->getData())){
-                    $em=$this->getDoctrine()->getManager();
-                    $em->persist($contact);
-                    $em->flush();
-                    // Everything OK, redirect to wherever you want ! :
-
-                    return $this->redirectToRoute('core_contact');
-                }else{
-                    // An error ocurred, handle
-                    var_dump("Errooooor :(");
-                }
-            }
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Send mail
+            $this->sendEmail($contact);
+            return $this->redirectToRoute('core_contact');
         }
 
         return $this->render('@Core/pages/contact.html.twig', array(
@@ -70,34 +43,24 @@ class DefaultController extends Controller
         ));
     }
 
-    private function sendEmail($data){
-        $myappContactMail = 'phle2022@gmail.com';
-        $myappContactPassword = 'HACIENDA';
-
-        // In this case we'll use the ZOHO mail services.
-        // If your service is another, then read the following article to know which smpt code to use and which port
-        // http://ourcodeworld.com/articles/read/14/swiftmailer-send-mails-from-php-easily-and-effortlessly
-        $transport = \Swift_SmtpTransport::newInstance('smtp.gmail.com', 465,'ssl')
-            ->setUsername($myappContactMail)
-            ->setPassword($myappContactPassword);
-
-        $mailer = \Swift_Mailer::newInstance($transport);
+    /**
+     * Send email from contact form
+     * @param Contact $contact
+     */
+    private function sendEmail(Contact $contact){
 
         $message = \Swift_Message::newInstance()
             ->setSubject('Some Subject')
-            ->setFrom('phle2022@gmail.com')
-            ->setTo(array('jaymail04@gmail.com','solomon.grundy.51@gmail.com'))
+            ->setFrom(array($contact->getEmail() => 'Le Jardin dans la Vallée'))
+            ->setTo($this->getParameter('mailer_user'))
             ->setContentType("text/html")
-            ->setBody($this->renderView(':Email:contactMail.html.twig'));
+            ->setBody($this->renderView(':Email:contactMail.html.twig', array(
+                'contact' => $contact
+            )));
 
 # Send the message
-        $this->get('mailer')
-            ->send($message);
-
-        return $mailer->send($message);
+        $this->get('mailer')->send($message);
     }
-
-
 }
 
 
