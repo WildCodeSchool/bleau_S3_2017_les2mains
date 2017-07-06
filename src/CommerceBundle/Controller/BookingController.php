a572fb43b56df03a7400bb5ad8bb21ddc628fb2b
JIM2022 <jaymail04@gmail.com>
2017-07-06 10:20:42 +0200

    Commentaires Booking_Controller

--- src/CommerceBundle/Controller/BookingController.php
+++ src/CommerceBundle/Controller/BookingController.php
@@ -15,48 +15,74 @@ use Symfony\Component\HttpFoundation\Request;
 
 class BookingController extends Controller
 {
-
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function listEvenenementAction()
     {
+        // La liste des Evenements Crées en BDD
         $em = $this->getDoctrine()->getManager();
         $evenements = $em->getRepository(Evenement::class)->findAll();
 
+        // On retourne à la vue tous les évenements
         return $this->render('@Commerce/user/listAllEvenements.html.twig', array(
             'evenements'=> $evenements,
         ));
     }
 
+    /**
+     * @param Request $request
+     * @param Evenement $evenement
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function bookingAction(Request $request, Evenement $evenement)
     {
-/*        $em = $this->getDoctrine()->getManager();
-        $evenement = $em->getRepository(Evenement::Class)->findOneById($id);*/
+        // Méthode qui permet de définir(remplir) un panier.
+
+                //************************************************************//
+
+                    // Méthode qui remplace le typage strict Evenement,$evenement
+                    /*$em = $this->getDoctrine()->getManager();
+                      $evenement = $em->getRepository(Evenement::Class)->findOneById($id);*/
+
+
+                //************************************************************//
 
         $user = new User();
+        // Recupération des marchandises dans les évenements
         foreach ($evenement->getMarchandises() as $marchandise){
+            // Création d'un new objet de la table SelectProduit
             $selectProduit = new SelectProduit();
+            //Joindre des marchandises à $selectProduit
             $selectProduit->setMarchandise($marchandise);
+            // ajout de ts les produits crées dans la Var User
             $user->addSelectProduit($selectProduit);
         }
-
+            // Création d'un formulaire User(Panier)
         $formUser = $this->createForm(UserType::class, $user);
         $formUser->handleRequest($request);
 
 
-        if ($formUser->isSubmitted() && $formUser->isValid()) {
+        if ($formUser->isSubmitted() && $formUser->isValid())
+        {
+            // Connexion à la BDD
             $em = $this ->getDoctrine()->getManager();
+            // Stockage des informations dans la Var $user
             $em->persist($user);
 
-            foreach ($user->getSelectProduits() as $selectProduit){
+        //  SelectProduit Attribué à User à chaque tour de boucle
+            foreach ($user->getSelectProduits() as $selectProduit)
+            {
                 $selectProduit->setUser($user);
             }
             $em->persist($user);
-
+        // Conservation en Mémoire Temp des infos de $user
             $session = $request->getSession();
             $session->set('panier', $user);
-
+        // Je retourne à la vue 'Recap' mon panier ($user) si ce dernier est valide
             return $this->render('@Commerce/user/recap.html.twig');
         }
-
+        // Sinon retour à la vue de notre formulaire
         return $this->render('@Commerce/user/booking.html.twig', array(
             'evenement' => $evenement,
             'formUser' => $formUser->createView()
@@ -64,8 +90,13 @@ class BookingController extends Controller
 
     }
 
+    /**
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function addBookingAction(Request $request)
     {
+        //Création des formulaires dans la vue Admin add_Booking
         $evenement = new Evenement();
 
         for ($i = 0; $i < 3; $i++){
