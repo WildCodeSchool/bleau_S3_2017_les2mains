06f619a72231a8fb191bb1f02007e197735b2916
Florian Grandjean <florian.pdf@gmail.com>
2017-08-08 22:29:11 +0200

    Fix bug floatType

--- src/CommerceBundle/Form/CustomType/FloatType.php
+++ src/CommerceBundle/Form/CustomType/FloatType.php
@@ -5,10 +5,23 @@
 namespace CommerceBundle\Form\CustomType;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\DataTransformer\IntegerToLocalizedStringTransformer;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class FloatType extends AbstractType
 {
+	public function configureOptions(OptionsResolver $resolver)
+	{
+		$resolver->setDefaults(array(
+			'attr' => array(
+				'step' => "any"
+			),
+			"scale" => 2,
+			"rounding_mode" => IntegerToLocalizedStringTransformer::ROUND_DOWN
+		));
+	}
+
 	public function getParent()
 	{
 		return NumberType::class;
